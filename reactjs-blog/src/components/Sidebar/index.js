11009d1f45b432e1972c5217f492866c74ab0173
Vinh Le Ngoc <vinhln@imt-soft.com>
2020-06-05 10:11:25 +0700

    load data from mokup

---
+++ reactjs-blog/src/components/Sidebar/index.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from 'react';
+import './style.css';
+import Card from '../UI/Card';
+import blogPost from "../../data/blog.json";
+import { NavLink } from 'react-router-dom';
+
+/**
+* @author
+* @function Sidebar
+**/
+
+const Sidebar = (props) => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {    
+    const posts = blogPost.data;
+    setPosts(posts);
+  }, posts);
+
+  return(
+    <div className="sidebarContainer">
+      <Card style={{marginBottom: '20px', padding: '20px', boxSizing: 'border-box'}}>
+        <div className="cardHeader">
+          <span>About us</span>
+        </div>
+        <div className="profileImageContainer">
+          <img src={require("../../blogPostImages/about-us.jpg")} alt="about-us" />
+        </div>
+        <div className="cardBody">
+          <p className="personalBio">Meh synth Schlitz, tempor duis single-origin coffee ea next level ethnic fingerstache 
+            fanny pack nostrud. Photo booth anim 8-bit hella, PBR 3 wolf moon beard Helvetica.
+          </p>
+        </div>
+      </Card>
+
+      <Card style={{marginBottom: '20px', padding: '20px', boxSizing: 'border-box'}}>
+        <div className="cardHeader">
+          <span>Social Network</span>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="cardHeader">
+          <span>Recent Post</span>
+        </div>
+        <div className="recentPosts">
+
+          {
+            posts.map(post => {
+              return (
+                <NavLink to={`/post/${post.id}`}>
+                  <div className="recentPost">  
+                    <h3>{post.blogTitle}</h3>
+                    <span>{post.postedOn}</span>
+                  </div>
+                </NavLink>
+              )
+            })
+          }
+        </div>
+      </Card>
+    </div>
+   )
+
+ }
+
+export default Sidebar
